c81e46287f667126dee20b9cebb38f01f79a3b9d
Alexander Mandt <alexander.mandt@outlook.at>
2019-08-25 09:35:41 +0200

    Changes

--- js/main.js
+++ js/main.js
@@ -2,15 +2,12 @@ let  yourDate = "2019-08-26";
 
 let currentDate = moment();
 let destinationDate = moment(yourDate,'YYYY-MM-DD');
-let timeDifference = moment.duration(destinationDate.diff(currentDate,"seconds"));
+let timeDifference = destinationDate.diff(currentDate,"seconds");
 
-let clock = $('.your-clock').FlipClock({
+let clock = $('.your-clock').FlipClock(timeDifference,{
 	clockFace: 'DailyCounter',
 	clockFaceOptions: {
 		countdown: true,
 		language: 'german'
 	}
 });
-
-clock.stop().setFaceValue(destinationDate.diff(currentDate, 's'))
-.start();
