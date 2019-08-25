651c3a0c224be4f12655e2385cd55a835dca94a7
Alexander Mandt <alexander.mandt@outlook.at>
2019-08-25 09:30:54 +0200

    Added Changes
    Signed-off-by: Alexander Mandt <alexander.mandt@outlook.at>

--- js/main.js
+++ js/main.js
@@ -4,8 +4,13 @@ let currentDate = moment();
 let destinationDate = moment(yourDate,'YYYY-MM-DD');
 let timeDifference = moment.duration(destinationDate.diff(currentDate,"seconds"));
 
-let clock = $('.your-clock').FlipClock(timeDifference.asSeconds(), {
+let clock = $('.your-clock').FlipClock({
 	clockFace: 'DailyCounter',
-	countdown: true,
-	language: 'german'
+	clockFaceOptions: {
+		countdown: true,
+		language: 'german'
+	}
 });
+
+clock.stop().setFaceValue(destinationDate.diff(currentDate, 's'))
+.start();
