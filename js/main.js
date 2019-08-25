1af808249587154c946edb06b621e76b3441a4a2
Alexander Mandt <alexander.mandt@outlook.at>
2019-08-25 09:18:46 +0200

    Added Date COnfiguration

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
-var clock = $('.your-clock').FlipClock(3600*24*3, {
+let  yourDate = "2019-08-26";
+
+let currentDate = moment();
+let destinationDate = moment(yourDate,'YYYY-MM-DD');
+let timeDifference = moment.duration(destinationDate.diff(currentDate,"seconds"));
+
+let clock = $('.your-clock').FlipClock(timeDifference.asSeconds(), {
 	clockFace: 'DailyCounter',
 	countdown: true,
 	language: 'german'
